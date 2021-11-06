e965380148a7f7a4f2e8c9b414df0e95c7a1d5bf
inhye6-6 <haine4546@gmail.com>
2021-11-06 11:14:33 +0900

    [211105] face undetected issue

--- authentication/face_authentication.py
+++ authentication/face_authentication.py
@@ -13,12 +13,13 @@ from asgiref.sync import sync_to_async
 
 
 def authentication(image,embedding,confidence=0.5):
-
     
-    face = detect_face(image, confidence)
-
-    face_to_recog = face
-    face = cv2.resize(face, (32, 32))
+    try:
+        face = detect_face(image, confidence)
+        face_to_recog = face
+        face = cv2.resize(face, (32, 32))
+    except:
+        return "Undetected"
 
     face = face.astype('float') / 255.0
     face = np.array(face,dtype='float32')
